1addd6881077ea6371aea1d42bdb25d4a258093e
YulianaTesting <testing.qa0099@gmail.com>
2025-10-16 20:59:36 +0300

    user

---
+++ src/test/java/ui_tests/LoginTests.java
@@ -0,0 +1,32 @@
+package ui_tests;
+
+import dto.User;
+import manager.ApplicationManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import pages.LoginPage;
+
+public class LoginTests extends ApplicationManager {
+
+    @Test
+    public void loginPositiveTest(){
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginForm("a@mail.ru", "Password123!");
+    }
+
+    @Test
+    public void loginNegativeTest_wrongPassword(){
+        User user = new User("a@mail.ru","password4321");
+        HomePage homePage = new HomePage(getDriver());
+        homePage.clickBtnLoginHeader();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.typeLoginFormWithUser(user);
+    }
+
+
+}
+
